5c5f95610231a84564ecae2a4e2df789614462d6
MCole-K9 <m.b.cole300@gmail.com>
2023-09-26 11:15:42 -0500

    Some Annotaions

--- src/lib/server/party/index.ts
+++ src/lib/server/party/index.ts
@@ -1,21 +1,22 @@
 import type * as Party from "partykit/server";
 
 export default class Server implements Party.Server {
-  constructor(readonly party: Party.Party) {}
 
+  // can access to Party's state within class with 'this.party'
+  constructor(readonly party: Party.Party) { }
+
+  // runs when a connection JOINS the party
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
     // A websocket just connected!
     console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
+      `Connected: id: ${conn.id} room: ${this.party.id} url: ${new URL(ctx.request.url).pathname}`
     );
 
     // let's send a message to the connection
     conn.send("hello from server");
   }
-
+  
+ // runs when a connection SENDS a message using 'socket.send(msg)'
   onMessage(message: string, sender: Party.Connection) {
     // let's log the message
     console.log(`connection ${sender.id} sent message: ${message}`);
