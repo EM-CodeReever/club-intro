8187fb14f885b62e09a4efb87bdc813a821cb8ea
MCole-K9 <m.b.cole300@gmail.com>
2023-09-28 00:28:04 -0500

    Qr Code

---
+++ src/routes/qr/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+
+<div class="flex justify-center h-[90vh]">
+    
+<img src="site-qr.png" alt="QR CODE" class="m-auto">
+
+</div>
